ce102c3da08ce2a34b8407016e89b654594e1584
EC2 Default User <ec2-user@ip-172-31-34-56.ec2.internal>
2022-02-07 09:07:11 +0000

    [Add]profile_image

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,22 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = @user.books
   end
 
   def index
     @books = Book.all
+    @user = User.find(current_user.id)
+    @book = @user.books
+
   end
-  
-  
+
+
   def edit
   end
-  
+
 end
