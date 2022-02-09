0611f42237ea5dd3ddf7eb3092dcf49b0c31dcdc
EC2 Default User <ec2-user@ip-172-31-34-56.ec2.internal>
2022-02-09 08:29:11 +0000

    [Add]book#update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,9 +23,14 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
-
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
 # 投稿データのストロングパラメータ
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
 
 
-  resources :books, only: [:new, :index, :show, :edit]
+  resources :books, only: [:new, :index, :show, :edit, :create, :update]
   resources :users, only: [:index, :show, :edit]
 
 
